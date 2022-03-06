9bf73af567abbbc85de07d1959d152c14384ff83
eliasbw <73832886+eliasbw@users.noreply.github.com>
2022-03-06 14:26:51 +0100

    Add files via upload
    Updated files

--- src/PortalConnection.java
+++ src/PortalConnection.java
@@ -42,7 +42,7 @@ public class PortalConnection {
                 "INSERT INTO REGISTRATIONS VALUES(?,?)");){
             ps.setString(1,student);
             ps.setString(2, courseCode);
-            ps.executeUpdate();
+            int n1 = ps.executeUpdate();
             return "{\"success\":true\"}";
 
         } catch (SQLException e) {
@@ -57,54 +57,75 @@ public class PortalConnection {
       // }     
     }
 
-    // Unregister a student from a course, returns a tiny JSON document (as a String)
-    public String unregister(String student, String courseCode) {
-        //DELETE FROM Registrations WHERE Student = '1111111111' AND course = 'CCC333';
 
-        try (PreparedStatement ps2 = conn.prepareStatement(
-                "DELETE FROM REGISTRATIONS WHERE student = ? AND course = ?");) {
-            ps2.setString(1, student);
-            ps2.setString(2, courseCode);
-            int n = 0;
-            n = ps2.executeUpdate();
-            if (n >= 1) {
-                System.out.println("Deleted object" + student + ',' + courseCode);
+
+
+
+    public String unregister(String student, String courseCode) {
+
+        String query = "DELETE FROM REGISTRATIONS WHERE Student ='"+student+ "'AND course = '"+courseCode+"'";
+        try (PreparedStatement ps = conn.prepareStatement(query);){
+
+            int n = ps.executeUpdate();
+            System.out.println(n);
+            System.out.println("query is " + ps);
+            if(n >= 1) {
+                System.out.println("Deleted object " + student + ',' + courseCode);
                 return "{\"success\":true\"}";
             }
-            else {
-                System.out.println("Student is not in registrations");
-                return "{\"success\":false\"}";
-            }
+            System.out.println("Student is not in registrations");
+            return "{\"success\":false\"}";
+
+
+
         } catch (SQLException e) {
-            return "{\"success\":false, \"error\":\""+ getError(e)+"\"}";
+            return "{\"success\":false, \"error\":\"" + getError(e) + "\"}";
         }
-
-
-        //return "{\"success\":false, \"error\":\"Unregistration is not implemented yet :(\"}";
-
     }
+
+
+
+
 
 
 
     // Return a JSON document containing lots of information about a student, it should validate against the schema found in information_schema.json
     public String getInfo(String student) throws SQLException{
-        
+
+
         try(PreparedStatement st = conn.prepareStatement(
-            // replace this with something more useful
-            "SELECT jsonb_build_object('student',idnr,'name',name) AS jsondata FROM BasicInformation WHERE idnr=?"
-            );){
+                "SELECT jsonb_agg(jsonb_build_object('sID',idnr,'name',Basicinformation.name,'login',login,'program',program,'branch',branch,'finished',(SELECT jsonb_agg(jsonb_build_object('course', Courses.name,'code',course,'credits',FinishedCourses.credits,'grade',grade))\n" +
+                        "\t\t\tFROM FinishedCourses, Courses WHERE Student = Basicinformation.idnr AND Courses.code= FinishedCourses.course) \n" +
+                        "\t\t\n" +
+                        "\t\t\t,'registered'\t,(SELECT (jsonb_agg(jsonb_build_object('coursename',Courses.name,'code',course,'status',status )))\n" +
+                        "\t\t\t\t\t\tFROM Registrations, Courses WHERE Student = Basicinformation.idnr AND Courses.code = Registrations.course)\n" +
+                        "\t\t\t\t\t\t\n" +
+                        "\t\t\t\t\t\t,'seminarCourses', (SELECT seminarCourses from pathtograduation WHERE student = Basicinformation.idnr)\n" +
+                        "\t\t\t\t\t\t\n" +
+                        "\t\t\t\t\t\t,'mathCredits', (SELECT mathCredits from pathtograduation WHERE student = Basicinformation.idnr)\n" +
+                        "\t\t\t\t\t\t\n" +
+                        "\t\t\t\t\t\t,'researchCredits', (SELECT researchCredits from pathtograduation WHERE student = Basicinformation.idnr)\n" +
+                        "\t\t\t\t\t\t\n" +
+                        "\t\t\t\t\t\t,'totalCredits', (SELECT totalCredits from pathtograduation WHERE student = Basicinformation.idnr)\n" +
+                        "\t\t\t\t\t\t\n" +
+                        "\t\t\t\t\t\t,'canGraduate', (SELECT qualified from pathtograduation WHERE student = Basicinformation.idnr)\n" +
+                        "\t\t\t\t\t\t\t\t\n" +
+                        "\t\t\t\t\t\t))AS student_data\n" +
+                        "\t\tFROM Basicinformation WHERE idnr = ? ");){
             
             st.setString(1, student);
-            
+
             ResultSet rs = st.executeQuery();
             
             if(rs.next())
-              return rs.getString("jsondata");
+              return rs.getString("student_data");
             else
               return "{\"student\":\"does not exist :(\"}"; 
             
         } 
     }
+
+
 
     // This is a hack to turn an SQLException into a JSON string error message. No need to change.
     public static String getError(SQLException e){
--- src/TestPortal.java
+++ src/TestPortal.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 public class TestPortal {
 
    // enable this to make pretty printing a bit more compact
@@ -7,73 +9,125 @@ public class TestPortal {
 
    public static void main(String[] args) {
       try{
-         PortalConnection c = new PortalConnection();
-   
-         // Write your tests here. Add/remove calls to pause() as desired. 
-         // Use println instead of prettyPrint to get more compact output (if your raw JSON is already readable)
+        PortalConnection c = new PortalConnection();
+          System.out.println("Unregister student from limited course with queue");pause();
+          System.out.println(c.unregister("1111111111", "CCC333"));
+
+        System.out.println("Get info about student\n"+c.getInfo("2222222222"));
+        TimeUnit.SECONDS.sleep(1);
+
+        System.out.println("Register student to course"); pause();
+        System.out.println(c.register("2222222222", "CCC111"));
+
+          System.out.println("Get info about student");pause();
+        System.out.println(c.getInfo("2222222222"));
+
+        System.out.println("Register student to same course again");pause();
+         System.out.println(c.register("2222222222", "CCC111"));
+
+          System.out.println("Unregister student form course"); pause();
+          System.out.println(c.unregister("2222222222", "CCC111"));
+
+          System.out.println("Unregister student form course again");pause();
+          System.out.println(c.unregister("2222222222", "CCC111"));
+
+          System.out.println("Register student to course without having the prerequisite");pause();
+          System.out.println(c.register("2222222222", "CCC555"));
+
+          System.out.println("Unregister student from limited course with queue");pause();
+          System.out.println(c.unregister("1111111111", "CCC333"));
+
+          System.out.println("Register student from limited course with queue");pause();
+          System.out.println(c.register("1111111111", "CCC333"));
+
+          System.out.println("Unregister student from limited course with queue");pause();
+            System.out.println(c.unregister("1111111111", "CCC333"));
+
+          System.out.println("Register student from limited course with queue");pause();
+          System.out.println(c.register("1111111111", "CCC333"));
+
+          System.out.println("Unregister student form overfull course");pause();
+          System.out.println(c.unregister("1111111111", "CCC222"));
+
+          System.out.println("Remove all student via SQL injection");pause();
+          System.out.println(c.unregister("1111111111", "test' OR 't1'='t1"));
+          /*
+            -List info for a student.
+            -Register a student for an unrestricted course, and check that he/she ends up registered (print info again).
+            -Register the same student for the same course again, and check that you get an error response.
+            -Unregister the student from the course, and then unregister him/her again from the same course. Check that the student is no longer registered and that the second unregistration gives an error response.
+            -Register the student for a course that he/she doesn't have the prerequisites for, and check that an error is generated.
+            -Unregister a student from a restricted course that he/she is registered to, and which has at least two students in the queue. Register again to the same course and check that the student gets the correct (last) position in the waiting list.
+            -Unregister and re-register the same student for the same restricted course, and check that the student is first removed and then ends up in the same position as before (last).
+            Unregister a student from an overfull course, i.e. one with more students registered than there are places on the course (you need to set this situation up in the database directly). Check that no student was moved from the queue to being registered as a result.
+            Unregister with the SQL injection you introduced, causing all (or almost all?) registrations to disappear.
+             */
 
 
-         // System.out.println(c.register("6666666666", "CCC111"));
-        //  pause();
-
-
-          System.out.println(c.unregister("1111111111", "CCC111"));
-         pause();
-/*
-         prettyPrint(c.getInfo("2222222222")); 
-         pause();
-
-
-         prettyPrint(c.getInfo("2222222222"));
 
 
 
-  */
+
+
+
+
+
+
+          //DELETE FROM Waiting WHERE position=1 AND course='x'' OR ''a''=''a'
+
+
+         // String code1 = "x' OR 'a'='a";
+         // DELETE FROM Waiting WHERE position=1 AND course='x'' OR ''a''=''a'
+      //      System.out.println(c.unregister("1111111111", "test' OR 't1'='t1"));
+        //    pause();
+
+
+
       } catch (ClassNotFoundException e) {
          System.err.println("ERROR!\nYou do not have the Postgres JDBC driver (e.g. postgresql-42.2.18.jar) in your runtime classpath!");
       } catch (Exception e) {
          e.printStackTrace();
       }
    }
-   
-   
-   
+
+
+
    public static void pause() throws Exception{
      System.out.println("PRESS ENTER");
      while(System.in.read() != '\n');
    }
-   
-   // This is a truly horrible and bug-riddled hack for printing JSON. 
+
+   // This is a truly horrible and bug-riddled hack for printing JSON.
    // It is used only to avoid relying on additional libraries.
    // If you are a student, please avert your eyes.
    public static void prettyPrint(String json){
       System.out.print("Raw JSON:");
       System.out.println(json);
       System.out.println("Pretty-printed (possibly broken):");
-      
+
       int indent = 0;
       json = json.replaceAll("\\r?\\n", " ");
       json = json.replaceAll(" +", " "); // This might change JSON string values :(
       json = json.replaceAll(" *, *", ","); // So can this
-      
+
       for(char c : json.toCharArray()){
         if (c == '}' || c == ']') {
           indent -= 2;
           breakline(indent); // This will break string values with } and ]
         }
-        
+
         System.out.print(c);
-        
+
         if (c == '[' || c == '{') {
           indent += 2;
           breakline(indent);
-        } else if (c == ',' && !COMPACT_OBJECTS) 
+        } else if (c == ',' && !COMPACT_OBJECTS)
            breakline(indent);
       }
-      
+
       System.out.println();
    }
-   
+
    public static void breakline(int indent){
      System.out.println();
      for(int i = 0; i < indent; i++)
